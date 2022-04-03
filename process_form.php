680d6a75953660524913385c73c8b178202f7a3d
Andres Soca <lumen.development01@gmail.com>
2022-04-03 14:38:14 -0400

    minor tweaks

--- process_form.php
+++ process_form.php
@@ -7,13 +7,13 @@ $message = $_REQUEST['message'];
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-$mail_body = "Name : ".$name."<br>"; 
+$mail_body = "Name : ".$name."<br>";
 $mail_body .= "Email : " . $email . "<br>";
 $mail_body .= "Subject : " . $subject . "<br>";
 $mail_body .= "Message : " . $message . "<br>";
-if(mail("youremail@mail.com","From: Contact Form Your name",$mail_body,$headers)){
+if(mail("asoca783@gmail.com","From: Contact Form Your name",$mail_body,$headers)){
     $thanks_mail_body = "Hello " . $name ."<br>";
-    
+
     $thanks_mail_body .= "Hey! Thanks for your email!<br>";
     $thanks_mail_body .= "I will get back to you soon.<br>";
     $thanks_mail_body .= "Thank You,<br>";
@@ -23,7 +23,7 @@ if(mail("youremail@mail.com","From: Contact Form Your name",$mail_body,$headers)
         echo "1";
         die();
     }
-    echo "1";    
+    echo "1";
 }else{
     echo "0";
 }
